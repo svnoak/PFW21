06518e5cda105f70c253e4e182ae199b6bfee97c
Matilda <matildaeinar@gmail.com>
2021-05-10 12:27:11 +0200

    countryElements in js, first draft

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let countryElementContainer = document.querySelector('.country-cards-container');
+
+createCountryElement(DB.COUNTRIES)
+
+function createCountryElement(countries) {
+    countries.forEach(country => {
+        let countryElement = document.createElement('div');
+        countryElement.classList.add('countryElement');
+
+        let countryName = document.createElement('h3');
+        countryName.textContent = country.name;
+
+        let countryInfo = document.createElement('p');
+        countryInfo.textContent = country.text;
+
+        let countryImg = document.createElement('div');
+        countryImg.style.backgroundImage = `url(assets/images/${country.imagesNormal[1]})`;
+
+        let showProgramsButton = document.createElement('button');
+        countryImg.append(showProgramsButton);
+        
+        
+        countryElement.append(countryName, countryInfo, countryImg);
+        countryElementContainer.append(countryElement);
+
+        // render(countryElement, '.country-cards-container')
+    });
+}
