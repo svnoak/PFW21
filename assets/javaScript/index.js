193a27dc5cb75bca4713621eb1af3ad99d99ff47
Matilda <matildaeinar@gmail.com>
2021-05-10 14:34:47 +0200

    created wrapper with all it's inner elements

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,10 +1,72 @@
 "use strict";
 
-createCountryElement(DB.COUNTRIES);
+render(createWrapper(), 'body');
 
+function createWrapper() {
+    let wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+
+    wrapper.append(createHero(),
+    createBrandInfo(),
+    createAdSpace(),
+    createCountryElement(DB.COUNTRIES));
+
+    return wrapper;
+}
+
+// Skapar HERO med innehåll
+function createHero() {
+    let indexHero = document.createElement('div');
+    indexHero.className = 'hero';
+
+    let titleHero = document.createElement('h1');
+    titleHero.className = 'title-hero';
+    titleHero.textContent = 'Upptäck världen som student.'
+
+    let globeRender = document.createElement('div');
+    globeRender.className = 'globe-render';
+
+    let textHero = document.createElement('p');
+    textHero.className = 'text-default bold';
+    textHero.textContent = 'Här kan du hitta spännande program över hela världen. Vi hjälper dig att hitta rätt!';
+
+    let buttonHero = document.createElement('button');
+    buttonHero.className = 'button-large button-round button-cta';
+    buttonHero.textContent = 'Sök efter program';
+
+    indexHero.append(titleHero, globeRender, textHero, buttonHero);
+    
+    return indexHero;
+}
+
+function createBrandInfo() {
+    let brandInfo = document.createElement('div');
+    brandInfo.className = 'brand-info';
+
+    let title = document.createElement('h2');
+    title.className = 'title-default';
+    title.textContent = 'VILKA ÄR [BRAND]?';
+
+    let text = document.createElement('p');
+    text.className = 'text-default';
+    text.textContent = '[Brand] erbjuder en lättanvänd sökmotor för dig som vill eller funderar på att studera utomlands. Filtrera utefter dina behov och jämför dina favoriter.';
+
+    brandInfo.append(title, text);
+    return brandInfo;
+}
+
+function createAdSpace() {
+    let adSpace = document.createElement('div');
+    adSpace.className = 'ad-space';
+
+    return adSpace;
+}
 
 // Skapar DOM-element för alla länder
 function createCountryElement(countries) {
+    let countryCardsContainer = document.createElement('div');
+    countryCardsContainer.className = 'country-cards-container';
+
     countries.forEach(country => {
         let countryElement = document.createElement('div');
         countryElement.className = 'countryElement';
@@ -27,7 +89,9 @@ function createCountryElement(countries) {
         countryImg.append(showProgramsButton);
         
         
-        countryElement.append(countryName, countryInfo, countryImg);
-        render(countryElement, 'country-cards-container');
+        countryElement.append(countryName, countryInfo, countryImg); 
+        countryCardsContainer.append(countryElement);
     });
+
+    return countryCardsContainer;
 }
