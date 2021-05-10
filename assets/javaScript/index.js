92cbeed185e389a7ee34b4064f90ffab4f178206
Matilda <matildaeinar@gmail.com>
2021-05-10 12:46:49 +0200

    updated countryCards
    added classes and appended element through render

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,30 +1,30 @@
 "use strict";
 
-let countryElementContainer = document.querySelector('.country-cards-container');
-
 createCountryElement(DB.COUNTRIES)
 
 function createCountryElement(countries) {
     countries.forEach(country => {
         let countryElement = document.createElement('div');
-        countryElement.classList.add('countryElement');
+        countryElement.className = 'countryElement';
 
         let countryName = document.createElement('h3');
+        countryName.className = 'title-default';
         countryName.textContent = country.name;
 
         let countryInfo = document.createElement('p');
+        countryInfo.className = 'text-small';
         countryInfo.textContent = country.text;
 
         let countryImg = document.createElement('div');
         countryImg.style.backgroundImage = `url(assets/images/${country.imagesNormal[1]})`;
 
         let showProgramsButton = document.createElement('button');
+        showProgramsButton.innerHTML = 'Visa program';
+        showProgramsButton.className = 'button-medium button-cta button-round text-small'
         countryImg.append(showProgramsButton);
         
         
         countryElement.append(countryName, countryInfo, countryImg);
-        countryElementContainer.append(countryElement);
-
-        // render(countryElement, '.country-cards-container')
+        render(countryElement, 'country-cards-container')
     });
 }
