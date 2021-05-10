6df51fe0f0a29baf9189f992eb9ea17971122f43
Matilda <matildaeinar@gmail.com>
2021-05-10 13:12:08 +0200

    small changes on CountryElements

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -14,17 +14,18 @@ function createCountryElement(countries) {
         let countryInfo = document.createElement('p');
         countryInfo.className = 'text-small';
         countryInfo.textContent = country.text;
+        countryInfo.innerHTML = `${countryInfo.innerHTML.substring(0,70)}...`;
 
         let countryImg = document.createElement('div');
         countryImg.style.backgroundImage = `url(assets/images/${country.imagesNormal[1]})`;
 
         let showProgramsButton = document.createElement('button');
-        showProgramsButton.innerHTML = 'Visa program';
-        showProgramsButton.className = 'button-medium button-cta button-round text-small'
+        showProgramsButton.className = 'button-large button-square text-small'
+        showProgramsButton.textContent = 'Visa program';
         countryImg.append(showProgramsButton);
         
         
         countryElement.append(countryName, countryInfo, countryImg);
-        render(countryElement, 'country-cards-container')
+        render(countryElement, 'country-cards-container');
     });
 }
