2962b35a1aa0b739530f99a738019e9085fe79d0
melsowole <82158719+melsowole@users.noreply.github.com>
2021-05-11 12:20:51 +0200

    makeHero() finished
    also added svgs.js to details.html

--- assets/javaScript/detail.js
+++ assets/javaScript/detail.js
@@ -8,6 +8,8 @@ let pID = DB.PROGRAMMES[0].id; // Simulate extraction from URL
 
 const detailedProgram = getProgrammeFromProgramID(pID);
 const detailedProgramUniversity = getUniversityFromUniID(detailedProgram.universityID);
+const detailedProgramCity = getCityFromUniID(detailedProgram.universityID);
+const detailedProgramCountry = getCountryFromUniID(detailedProgram.universityID)
 
 function makeHero(){
     let wrapper = document.createElement("section");
@@ -19,15 +21,41 @@ function makeHero(){
     studyInfo.className = ``;
     wrapper.append(header, studyInfo);
 
+    //header content
     let title = document.createElement("h1");
     title.textContent = detailedProgram.name;
     let subtitle =  document.createElement("span");
     subtitle.textContent = detailedProgramUniversity.name;
     header.append(title, subtitle);
+
+    // studyInfo content
+        const information = [
+            //location
+            [pinIcon, detailedProgramCity.name + ", " + detailedProgramCountry.name],
+            //langauge
+            [bookIcon, DB.LANGUAGES.find( language => language.id === detailedProgram.language).name],
+            // level
+            [bookOpenIcon, DB.LEVELS[detailedProgram.level] ],
+            // visa
+            [alertIcon, detailedProgramCountry.visa === true ? "Kräver visum" : "Kräver inte visum"]
+        ]
+
+    information.forEach(category => {
+        let wrapper = document.createElement("div");
+
+        let icon = document.createElement("i");
+        icon.innerHTML = category[0];
+        let text = document.createElement("span");
+        text.textContent = category[1];
+        wrapper.append(icon, text);
+
+        studyInfo.append(wrapper);
+    });
+
     
     return wrapper;
 }
 
-console.log( typeof makeHero() )
+document.body.append(makeHero());
 
-render( "body", makeHero() );
+// render( "body", makeHero() );
