9f08a03a7ea40a9a36382e5592ca0905d0528590
Matilda <matildaeinar@gmail.com>
2021-05-11 17:01:46 +0200

    börjat på sökfunktionen på jämförelsesidan

--- assets/javaScript/compare.js
+++ assets/javaScript/compare.js
@@ -1,1 +1,67 @@
 "use strict";
+
+render('body', createHeader());
+
+function createHeader() {
+    let header = document.createElement('div');
+    header.className = 'header';
+
+    let titleHeader = document.createElement('h1');
+    titleHeader.textContent = 'Jämför program';
+
+    let textHeader = document.createElement('p');
+    textHeader.textContent = 'Här kan du jämföra dina favoriter och bestämma vilket program som är rätt för just dig!';
+
+    let searchBar = document.createElement('input');
+    searchBar.type = 'list';
+    searchBar.placeholder = 'Lägg till program att jämföra';
+    searchBar.addEventListener('keyup', () => {
+        if (document.querySelector('.datalist')) {
+            document.querySelector('.datalist').remove();
+        }
+
+        let searchInput = searchBar.value.toLowerCase();
+        
+        let dataList = document.createElement('div');
+        dataList.className = 'datalist'
+        searchBar.after(dataList);
+        
+        getProgrammesBySearchWord(searchInput).forEach(program => {
+            let option = document.createElement('div');
+            option.textContent = program.name;
+            dataList.append(option);
+
+            option.addEventListener('click', () => {
+                addProgramToList(program.name);
+            });
+        });
+    });
+
+    let currentPrograms = document.createElement('div');
+
+
+    header.append(titleHeader, textHeader, searchBar, currentPrograms);
+
+    return header;
+}
+
+
+function getProgrammesBySearchWord(searchInput) {
+    let programmes = DB.PROGRAMMES.filter((obj) => {
+        let name = obj.name.toLowerCase();
+        return name.includes(searchInput);
+    });
+
+    // sortSearchResult(programmes);
+    return programmes;
+}
+
+function sortSearchResult(programmes) {
+    return programmes;
+}
+
+let addedProgrammes = [];
+
+function addProgramToList(programName) {
+    addedProgrammes.push(programName);
+}
