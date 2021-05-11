51a6dbd4ce3ef79c1e97be96652c5f8db1060fdd
melsowole <82158719+melsowole@users.noreply.github.com>
2021-05-11 10:56:55 +0200

    Created functions and variables to access university and program objects

--- assets/javaScript/detail.js
+++ assets/javaScript/detail.js
@@ -1,17 +1,33 @@
 "use strict";
+// add to global?
+function getProgrammeFromProgramID(programID){
+    return DB.PROGRAMMES.find( program => programID === program.id );
+}
+
+let pID = DB.PROGRAMMES[0].id; // Simulate extraction from URL 
+
+const detailedProgram = getProgrammeFromProgramID(pID);
+const detailedProgramUniversity = getUniversityFromUniID(detailedProgram.universityID);
 
 function makeHero(){
     let wrapper = document.createElement("section");
+    wrapper.className = ``;
     
     let header = document.createElement("header");
     header.className = ` `;
-    let studyInfo = document.createeElement("section");
+    let studyInfo = document.createElement("section");
     studyInfo.className = ``;
     wrapper.append(header, studyInfo);
-    
 
+    let title = document.createElement("h1");
+    title.textContent = detailedProgram.name;
+    let subtitle =  document.createElement("span");
+    subtitle.textContent = detailedProgramUniversity.name;
+    header.append(title, subtitle);
+    
+    return wrapper;
 }
 
-function render(element, parentElement) {
-    document.getElementById(parentElement).append(element);
-  }
+console.log( typeof makeHero() )
+
+render( "body", makeHero() );
