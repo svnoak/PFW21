c9c5dde5b603c65cf67c6a6a2f85ce1ef0ae54d6
Matilda <matildaeinar@gmail.com>
2021-05-19 11:47:40 +0200

    added trailing icon

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,11 +1,11 @@
 "use strict";
-
-render('body', createBackground(), createHero(), createBrandInfo(), createAdSpace(), cardCarousell(DB.COUNTRIES));
+render('body', createBackground());
+render('.wrapper', createHero(), createBrandInfo(), createAdSpace(), cardCarousell(DB.COUNTRIES), createCompareInfo());
 
 // Skapar HERO med innehåll
 function createHero() {
     let indexHero = document.createElement('div');
-    indexHero.className = 'hero';
+    indexHero.className = 'hero centered';
 
     let titleHero = document.createElement('h1');
     titleHero.className = 'title-hero';
@@ -15,13 +15,13 @@ function createHero() {
     globeRender.className = 'globe-render';
 
     let textHero = document.createElement('p');
-    textHero.className = 'text-small semi-bold';
+    textHero.className = 'text-default semi-bold';
     textHero.textContent = 'Här kan du hitta spännande program över hela världen. Vi hjälper dig att hitta rätt!';
 
     let buttonHero = document.createElement('a');
     buttonHero.href = 'search.html';
-    buttonHero.className = 'text-default semi-bold';
-    buttonHero.textContent = 'Sök efter program';
+    buttonHero.className = 'text-large semi-bold space-between button-round button-cta';
+    buttonHero.innerHTML = `<i id="white-icon">${searchIcon}</i> Sök efter program`;
 
     let pin = document.createElement('div');
     pin.classList = 'pin-header';
@@ -35,14 +35,14 @@ function createHero() {
 
 function createBrandInfo() {
     let brandInfo = document.createElement('div');
-    brandInfo.className = 'brand-info';
+    brandInfo.className = 'brand-info centered';
 
     let title = document.createElement('h2');
-    title.className = 'title-xsmall regular';
+    title.className = 'title-default regular';
     title.textContent = 'VILKA ÄR [BRAND]?';
 
     let text = document.createElement('p');
-    text.className = 'text-small light';
+    text.className = 'text-default light';
     text.textContent = '[Brand] erbjuder en lättanvänd sökmotor för dig som vill eller funderar på att studera utomlands. Filtrera utefter dina behov och jämför dina favoriter.';
 
     brandInfo.append(title, text);
@@ -65,7 +65,7 @@ function createCard(country) {
     header.className = 'country-info';
 
     let countryName = document.createElement('h3');
-    countryName.className = 'title-xsmall regular';
+    countryName.className = 'title-small regular';
     countryName.textContent = country.name;
 
     let countryInfo = document.createElement('p');
@@ -79,8 +79,8 @@ function createCard(country) {
     countryImg.className = 'country-img';
 
     let showProgramsButton = document.createElement('a');
-    showProgramsButton.className = 'text-default semi-bold'
-    showProgramsButton.textContent = 'Visa program';
+    showProgramsButton.className = 'text-default semi-bold space-between'
+    showProgramsButton.innerHTML = `<p>Visa program</p> <i id="trailing-icon">${trailingIcon}</i>`;
     showProgramsButton.href = 'search.html';
     showProgramsButton.addEventListener('click', () => {
         sessionStorage.setItem('countryId', country.id);
@@ -93,10 +93,44 @@ function createCard(country) {
     return wrapper;
 }
 
+function createCompareInfo(){
+    let wrapper = document.createElement('section');
+    wrapper.className = 'compare-info-section centered';
+
+    let title = document.createElement('h2');
+    title.className = 'title-default regular'
+    title.textContent = 'Beslutsångest?';
+
+    let text = document.createElement('p');
+    text.className = 'text-default regular';
+    text.textContent = 'Med jämförverktyget får du en snabb överblick av kurser och kan enkelt ställa dina favoriter mot varandra.';
+
+    let buttonContainer = document.createElement('div');
+    buttonContainer.className = 'c-button-container';
+    let button = document.createElement('a');
+    button.href = 'comapare.html';
+    button.className = 'text-default light space-between';
+    button.innerHTML = `<p>Jämför program</p><i>${trailingIcon}</i>`;
+    buttonContainer.append(button);
+
+    wrapper.append(title, text, buttonContainer)
+
+    return wrapper;
+}
+
 function createBackground() {
-    let background = document.createElement('div');
+    let background = document.createElement('section');
     background.className = 'background';
-    background.innerHTML = backgroundCircle;
+
+    let firstCircle = document.createElement('div');
+    firstCircle.className = 'first-circle';
+    firstCircle.innerHTML = backgroundCircle;
+
+    let secondCircle = document.createElement('div');
+    secondCircle.className = 'second-circle';
+    secondCircle.innerHTML = backgroundCircle;
+
+    background.append(firstCircle, secondCircle);
 
     return background;
 }
