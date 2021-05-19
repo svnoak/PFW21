410f8a8359ff8e1f138810a3493eef6a4f743ebf
svnoak <kimpfimp@posteo.net>
2021-05-19 11:13:42 +0200

    automatically updating favorites when adding to local storage

--- assets/javaScript/favorites.js
+++ assets/javaScript/favorites.js
@@ -1,3 +1,8 @@
 "use strict";
 
+window.addEventListener("storage", e => {
+  createProgrammeElements("favourites", parseFavoritesFromLS("object")),
+  false;
+});
+
 createProgrammeElements("favourites", parseFavoritesFromLS("object"));
