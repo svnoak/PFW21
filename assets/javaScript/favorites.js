b8f6d08dd0a85d03889ad4e03ff054243e4e5fe4
svnoak <kimpfimp@posteo.net>
2021-05-19 11:10:45 +0200

    swapp function with parseFavoritesFromLS instead

--- assets/javaScript/favorites.js
+++ assets/javaScript/favorites.js
@@ -1,9 +1,3 @@
 "use strict";
 
-let programmes = [];
-
-let programmIDs = JSON.parse(localStorage.favoriteProgrammes);
-programmIDs.forEach( id => programmes.push(getProgrammesById(id)));
-
-createProgrammeElements("favourites", programmes);
-
+createProgrammeElements("favourites", parseFavoritesFromLS("object"));
