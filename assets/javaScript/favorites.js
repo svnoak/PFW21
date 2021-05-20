bd2022c425d0f7263d91bded85f0480c1f7c4bb4
svnoak <kimpfimp@posteo.net>
2021-05-20 13:03:21 +0200

    remove redundant code

--- assets/javaScript/favorites.js
+++ assets/javaScript/favorites.js
@@ -1,14 +1,6 @@
 "use strict";
 
-/*function addEventListener() {
-  document.querySelectorAll(".bookmark").forEach(b => {
-  b.addEventListener("click", e => createProgrammeElements("favourites", parseFavoritesFromLS("object")));
-  console.log("clicked");
-})
-};*/
-
 createProgrammeElements("favourites", parseFavoritesFromLS("object"));
-//addEventListener();
 
 window.addEventListener("storage", e => {
   createProgrammeElements("favourites", parseFavoritesFromLS("object"));
