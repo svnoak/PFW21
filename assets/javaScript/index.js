604b70693b65f81fda63ef626152c216259875c3
Kim <kimpfimp@posteo.net>
2021-05-20 16:40:12 +0200

    make link to search site dynamic depending on country.id

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -84,10 +84,7 @@ function createCard(country) {
     let showProgramsButton = document.createElement('a');
     showProgramsButton.className = 'text-large semi-bold space-between button-solid--cream button-square';
     showProgramsButton.innerHTML = `<p>Visa program</p> <i class="trailing-icon centered">${trailingIcon}</i>`;
-    showProgramsButton.href = 'search.html';
-    showProgramsButton.addEventListener('click', () => {
-        sessionStorage.setItem('countryId', country.id);
-    });
+    showProgramsButton.href = `search.html?${country.id}`;
     countryImg.append(showProgramsButton);
     
     
