18d365029da0055500e5c500a4b18503788b733d
Matilda <matildaeinar@gmail.com>
2021-05-21 14:13:09 +0200

    Updaterade ikoner

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -83,7 +83,7 @@ function createCard(country) {
 
     let showProgramsButton = document.createElement('a');
     showProgramsButton.className = 'text-large semi-bold space-between button-solid--cream button-square';
-    showProgramsButton.innerHTML = `<p>Visa program</p> <i class="trailing-icon centered">${trailingIcon}</i>`;
+    showProgramsButton.innerHTML = `<p>Visa program</p> <i class="trailing-icon centered">${trailingIconRight}</i>`;
     showProgramsButton.href = 'search.html';
     showProgramsButton.addEventListener('click', () => {
         sessionStorage.setItem('countryId', country.id);
@@ -113,7 +113,7 @@ function createCompareInfo(){
     let button = document.createElement('a');
     button.href = 'comapare.html';
     button.className = 'text-large light space-between button-solid--cream button-square';
-    button.innerHTML = `<p>Jämför program</p><i class="centered">${trailingIcon}</i>`;
+    button.innerHTML = `<p>Jämför program</p><i class="centered">${trailingIconRight}</i>`;
     buttonContainer.append(button);
 
     let circleContainer = createBackgroundCircle();
