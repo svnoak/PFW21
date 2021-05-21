78e2cf6c85cfd4371dd90da31b7a3a28848a16f9
Kim <kimpfimp@posteo.net>
2021-05-21 18:43:03 +0200

    lowerCase not uppercase

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -89,7 +89,7 @@ function createCard(country) {
     let showProgramsButton = document.createElement('a');
     showProgramsButton.className = 'text-large semi-bold space-between button-solid--cream button-square';
     showProgramsButton.innerHTML = `<p>Visa program</p> <i class="trailing-icon centered">${trailingIconRight}</i>`;
-    showProgramsButton.href = `search.html?coID=${country.name}`;
+    showProgramsButton.href = `search.html?coID=${country.name.toLowerCase()}`;
     countryImg.append(showProgramsButton);
     
     
