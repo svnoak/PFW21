e2799c753b79ba9ffcf39c6a5ef0f605fd2057ce
Matilda <matildaeinar@gmail.com>
2021-05-24 17:28:06 +0200

    CSS till dialogrutan på favoritsidan

--- assets/javaScript/globals.js
+++ assets/javaScript/globals.js
@@ -400,7 +400,7 @@ function removeBookmark() {
   return new Promise( confirm => {
     swal({
       title: "Vill du radera favoriten?",
-      icon: "warning",
+      // icon: "warning",
       buttons: {
           cancel: {
             text: "Nej",
